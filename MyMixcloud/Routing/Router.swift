fbfbed1f3d6f7fc4c19348f028df1edcfba009c0
ryabchikova <eryabchikova@gmail.com>
2022-09-23 18:39:11 +0300

    rearrange

---
+++ MyMixcloud/Routing/Router.swift
@@ -0,0 +1,10 @@
+//
+//  AllRouters.swift
+//  MyMixcloud
+//
+//  Created by Elena Ryabchikova on 16.09.2022.
+//  Copyright © 2022 ryabchikova. All rights reserved.
+//
+
+
+final class Router: BaseRouter, RoutingTrait {}
